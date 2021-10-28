f0f53c92b0305ed402ff8d33aaa028cff21d5ce7
Jason Barr <jason@jasonsbarr.com>
2021-10-27 22:07:33 -0500

    Read strings in lexer

--- src/parser/lexer.js
+++ src/parser/lexer.js
@@ -1,5 +1,82 @@
 import moo from "moo";
 
+const isHexadecimalChar = (ch) => /[0-9a-fA-F]/.test(ch);
+
+/**
+ *
+ * @param {String} raw raw value from token match
+ * @return {String}
+ */
+const readString = (raw) => {
+  const temp = [...raw.slice(1, -1)];
+  let i = 0;
+
+  const readWhile = (pred) => {
+    let str = "";
+
+    while (pred(temp[i])) {
+      str += temp[i++];
+    }
+
+    return str;
+  };
+
+  const readEscape = (ch) => {
+    let str = "";
+    let seq = "";
+
+    if (ch == "n") {
+      str += "\n";
+    } else if (ch == "b") {
+      str += "\b";
+    } else if (ch == "f") {
+      str += "\f";
+    } else if (ch == "r") {
+      str += "\r";
+    } else if (ch == "t") {
+      str += "\t";
+    } else if (ch == "v") {
+      str += "\v";
+    } else if (ch == "0") {
+      str += "\0";
+    } else if (ch == "'") {
+      str += "'";
+    } else if (ch == '"') {
+      str += '"';
+    } else if (ch == "\\") {
+      str += "\\";
+    } else if (ch == "x") {
+      // is hexadecimal escape sequence
+      seq = readWhile(isHexadecimalChar);
+      str += String.fromCharCode(parseInt(seq, 16));
+    } else if (ch == "u") {
+      // is Unicode escape sequence
+      seq = readWhile(isHexadecimalChar);
+      str += String.fromCodePoint(parseInt(seq, 16));
+    }
+
+    return str;
+  };
+
+  let str = "";
+  let escaped = false;
+
+  while (i < temp.length) {
+    let ch = temp[i++];
+
+    if (escaped) {
+      str += readEscape(ch);
+      escaped = false;
+    } else if (ch === "\\") {
+      escaped = true;
+    } else {
+      str += ch;
+    }
+  }
+
+  return str;
+};
+
 const tokens = {
   WS: /[ \t]/u,
   NL: { match: /\r?\n|\r/u, lineBreaks: true },
@@ -43,7 +120,7 @@ const tokens = {
       )
     ),
   },
-  string: { match: /^\"(?:\\?+\s\S)*?\"$/u },
+  string: { match: /^\"(?:\\?+\s\S)*?\"$/u, value: (s) => readString(s) },
   apply: "->",
   pipe: "|>",
   concat: "++",
