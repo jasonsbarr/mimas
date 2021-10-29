480322108ee68acac581d2b2d1fba9a76f1d9a50
Jason Barr <jason@jasonsbarr.com>
2021-10-28 19:56:58 -0500

    Stub out type for AST

--- src/ast/ast.js
+++ src/ast/ast.js
@@ -1,2 +1,6 @@
 const { VariantInfo, createType } = require("../types/types");
 const { Record } = require("../types/record");
+
+const variantInfos = [VariantInfo("Program", [], {})];
+
+exports.Ast = createType("Ast", variantInfos);
