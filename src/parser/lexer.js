fcf9f976cd360805bb0f0819263a871a99db8bdb
Jason Barr <jason@jasonsbarr.com>
2021-10-31 20:21:22 -0500

    Remove rule for verbatim strings

--- src/parser/lexer.js
+++ src/parser/lexer.js
@@ -146,7 +146,6 @@ const tokens = {
   },
   string: [
     { match: /"""[\s\S]*"""/u, value: (s) => readString(s).slice(2, -2) },
-    { match: /@".*"@/u, value: (s) => String.raw`${s}`.slice(2, -2) },
     { match: /".*"/u, value: (s) => readString(s) },
   ],
   eof: { match: "<*endofinput*>", value: () => Symbol.for("end of input") },
