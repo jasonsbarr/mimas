7111bd015f0f43250f329e14f6bd484adbc77526
Jason Barr <jason@jasonsbarr.com>
2021-11-03 22:54:32 -0500

    Define env object and extend method

--- src/env/env.js
+++ src/env/env.js
@@ -1,1 +1,12 @@
-export const env = {};
+import { create } from "@jasonsbarr/functional-core/lib/object/create.js";
+
+export const env = (parent = null) => {
+  const envObj = {
+    vars: create(parent ? parent.vars : null),
+    types: create(parent ? parent.types : null),
+  };
+
+  envObj.extend = () => env(envObj);
+
+  return envObj;
+};
