5d1e140285951ef67b18f2f73fb33b69d3ed57d4
Jason Barr <jason@jasonsbarr.com>
2021-11-04 18:25:27 -0500

    Define environment methods and export constructor

--- src/env/env.js
+++ src/env/env.js
@@ -1,13 +1,82 @@
 import { create } from "@jasonsbarr/functional-core/lib/object/create.js";
+import { hasOwn } from "@jasonsbarr/functional-core/lib/object/hasOwn.js";
 
-export const env = (parent = null) => {
-  const envObj = {
-    vars: create(parent ? parent.vars : null),
-    types: create(parent ? parent.types : null),
-    parent,
-  };
+function Environment(parent = null) {
+  this.vars = create(parent ? parent.vars : null);
+  this.types = create(parent ? parent.types : null);
+  this.parent = parent;
+}
 
-  envObj.extend = () => env(envObj);
+Environment.prototype = {
+  extend() {
+    return new Environment(this);
+  },
 
-  return envObj;
+  lookup(name) {
+    let scope = this;
+    while (scope) {
+      if (hasOwn(name, scope.vars)) {
+        return scope;
+      }
+      scope = scope.parent;
+    }
+  },
+
+  lookupType(name) {
+    let scope = this;
+    while (scope) {
+      if (hasOwn(name, scope.types)) {
+        return scope;
+      }
+      scope = scope.parent;
+    }
+  },
+
+  lookupInCurrent(name) {
+    if (name in this.vars) {
+      return this.vars[name];
+    }
+    throw new ReferenceError(`${name} not found in current scope`);
+  },
+
+  lookupTypeInCurrent(name) {
+    if (name in this.types) {
+      return this.types[name];
+    }
+    throw new ReferenceError(`${name} not found in current scope`);
+  },
+
+  get(name) {
+    if (name in this.vars) {
+      return this.vars[name];
+    }
+    throw new ReferenceError(`Unbound identifier ${name}`);
+  },
+
+  getType(name) {
+    if (name in this.types) {
+      return this.types[name];
+    }
+    throw new ReferenceError(`Type ${name} not found`);
+  },
+
+  set(name, value) {
+    let scope = this.lookup(name);
+    if (!scope) {
+      throw new ReferenceError(
+        `Cannot set value for unbound identifier ${name}`
+      );
+    }
+    scope.vars[name] = value;
+  },
+
+  def(name, value) {
+    this.vars[name] = value;
+  },
+
+  defType(name, value) {
+    this.types[name] = value;
+  },
 };
+
+export const env = () => new Environment();
