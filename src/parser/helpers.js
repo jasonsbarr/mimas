dd437c4505e901d21e607f089edf0abadaf83477
Jason Barr <jason@jasonsbarr.com>
2021-11-22 19:55:56 -0600

    Knock out everything and start from scratch

--- src/parser/helpers.js
+++
@@ -1,3 +0,0 @@
-export const first = (arr) => (arr.length !== 0 ? arr[0] : null);
-
-export const last = (arr) => (arr.length !== 0 ? arr[arr.length - 1] : null);
--- src/parser/test.js
+++
@@ -1,6 +0,0 @@
-import parse from "./parser.js";
-
-const code = `if true then 1 else 0`;
-
-const parsed = parse(code);
-console.log(JSON.stringify(parsed, null, 2));
